48d5df3a251b973b0d6a103e1ea0426528b79a8f
Fasile24 <fasilek24@gmail.com>
2023-05-20 08:46:23 -0700

    inital commit

---
+++ src/test/java/com/noFoodWaste/NoFoodWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noFoodWaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoFoodWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
